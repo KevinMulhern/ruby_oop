50dabd2e332a612cc6106989a90a5f991db30422
Kevin Mulhern <kevinmulhern23@gmail.com>
2016-02-08 16:13:00 +0000

    Fixed indentation

--- tic_tac_toe/lib/board.rb
+++ tic_tac_toe/lib/board.rb
@@ -10,21 +10,21 @@ class Board
   end
 
   def winning_moves
-		[[0,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,7], [2,5,6], [0,4,8], [2,4,6]]
-	end
+    [[0,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,7], [2,5,6], [0,4,8], [2,4,6]]
+  end
 
   def valid_position?(pos)
-		pos >= 0 && pos <= 8 && positions[pos] == " "
-	end
+    pos >= 0 && pos <= 8 && positions[pos] == " "
+  end
 
   def set_position(position, symbol)
     positions[position] = symbol
-	end
+  end
 
   def all_positions_taken?
     !positions.include?(" ")
   end
-
+  
   def three_positions_in_row?(three_positions, symbol)
     three_positions.all? {|pos| positions[pos] == symbol }
   end
